4aea67c9921856ef147a63e5335ac17c274b3686
Natalia <alimadeoliveiranatalia@gmail.com>
2022-06-22 13:04:09 -0300

    feat: list courses, students, enrollments

--- classroom/src/http/auth/authorization.guard.ts
+++ classroom/src/http/auth/authorization.guard.ts
@@ -5,7 +5,8 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import jwt from 'express-jwt';
+import { GqlExecutionContext } from '@nestjs/graphql';
+import { expressjwt as jwt, GetVerificationKey } from 'express-jwt';
 import { expressJwtSecret } from 'jwks-rsa';
 import { promisify } from 'node:util';
 
@@ -19,9 +20,7 @@ export class AuthorizationGuard implements CanActivate {
     this.AUTH0_DOMAIN = this.configService.get('AUTH0_DOMAIN') ?? '';
   }
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const httpContext = context.switchToHttp();
-    const req = httpContext.getRequest();
-    const res = httpContext.getResponse();
+    const { req, res } = GqlExecutionContext.create(context).getContext();
 
     const checkJWT = promisify(
       jwt({
@@ -30,7 +29,7 @@ export class AuthorizationGuard implements CanActivate {
           rateLimit: true,
           jwksRequestsPerMinute: 5,
           jwksUri: `${this.AUTH0_DOMAIN}.well-known/jwks.json`,
-        }),
+        }) as GetVerificationKey,
         audience: this.AUTH0_AUDIENCE,
         issuer: this.AUTH0_DOMAIN,
         algorithms: ['RS256'],
