20635f208dc088a0fa21a71816e6f9429afe7103
Natalia <alimadeoliveiranatalia@gmail.com>
2022-06-22 06:56:38 -0300

    feat: module create and list products

--- purchases/src/http/auth/authorization.guard.ts
+++ purchases/src/http/auth/authorization.guard.ts
@@ -1,5 +1,11 @@
-import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { GqlExecutionContext } from '@nestjs/graphql';
 import { expressjwt as jwt, GetVerificationKey } from 'express-jwt';
 import { expressJwtSecret } from 'jwks-rsa';
 import { promisify } from 'node:util';
@@ -14,9 +20,7 @@ export class AuthorizationGuard implements CanActivate {
     this.AUTH0_DOMAIN = this.configService.get('AUTH0_DOMAIN') ?? '';
   }
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const httpContext = context.switchToHttp();
-    const req = httpContext.getRequest();
-    const res = httpContext.getResponse();
+    const { req, res } = GqlExecutionContext.create(context).getContext();
 
     const checkJWT = promisify(
       jwt({
